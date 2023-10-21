6a4b7812036bb8b16e6ffb22712c7bc4661128a6
Christoffer Hald Christensen <chrishald@outlook.dk>
2023-10-21 15:28:48 +0200

    Enemy wander randomly when player is out of sight

--- Assets/Scipts/Enemy/AI/NavMeshTest.cs
+++ Assets/Scipts/Enemy/AI/NavMeshTest.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class NavMeshTest : MonoBehaviour
 {
-    NavMeshAgent agent;
-
-    [SerializeField] Transform target;
+    [Header("Speed")]
+    [SerializeField] float huntingSpeed;
+    [SerializeField] float wanderSpeed;
+    [SerializeField] float huntingAcc;
+    [SerializeField] float wanderAcc;
+    
+    [Space(15)]
+    [Header("Misc.")]
     [SerializeField] float viewRange;
+    [SerializeField] float wanderDistance;
+    [SerializeField] Transform target;
+
+
+    NavMeshQueryFilter filter;
+    NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,20 +30,52 @@ public class NavMeshTest : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(UpdateVision());
+        filter.agentTypeID = agent.agentTypeID;
+        filter.areaMask = NavMesh.AllAreas;
+        StartCoroutine(ScanForPrey());
     }
 
-    IEnumerator UpdateVision()
+    private void Update()
+    {
+        if (Vector3.Distance(agent.destination, transform.position) < 2f)
+        {
+            print("Hello");
+
+            agent.speed = wanderSpeed;
+            agent.acceleration = wanderAcc;
+
+            Vector3 randomDirection = Random.insideUnitSphere * wanderDistance;
+            randomDirection += transform.position;
+
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomDirection, out hit, wanderDistance, filter))
+            {
+                agent.SetDestination(hit.position);
+                Debug.Log($"Set position. Distance is : {Vector3.Distance(agent.destination, transform.position)}");
+            }
+        }
+
+        print($"{agent.destination}");
+    }
+
+    IEnumerator ScanForPrey()
     {
         RaycastHit hit;
+
         while (true)
         {
             // Keep updating destination while in view
             if (Physics.Raycast(transform.position, target.position - transform.position, out hit, viewRange))
             {
-                agent.SetDestination(hit.point);
-                yield return new WaitForFixedUpdate();
-                continue;
+                if (hit.collider.CompareTag("Player"))
+                {
+                    agent.speed = huntingSpeed;
+                    agent.acceleration = huntingAcc;
+
+                    agent.SetDestination(hit.point);
+                    yield return new WaitForFixedUpdate();
+                    continue;
+                }
             }
 
             // Longer update times when out of view
