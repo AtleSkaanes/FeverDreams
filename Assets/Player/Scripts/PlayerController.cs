2b25f385a2016f05b649b273bff6d37ee16806d2
AtleSkaanes <atle.skaanes@gmail.com>
2023-10-23 10:44:32 +0200

    Changed system to inbuilt CharacterController

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Interactions;
 
-[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(PlayerInput), typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float playerMovemntSpeed = 1f;
 
     private PlayerInput input;
+    private CharacterController characterController;
+
 
     void Start()
     {
         input = GetComponent<PlayerInput>();
-        input.onWASD += new PlayerInput.OnWASD(MovePlayer);
+        input.onWASD += MovePlayer;
+
+        characterController = GetComponent<CharacterController>();
     }
 
     void MovePlayer(Vector2 movement)
     {
         Vector3 move = new Vector3(movement.x, 0, movement.y);
-        transform.Translate(move * playerMovemntSpeed * Time.deltaTime);
+        characterController.SimpleMove(move * playerMovemntSpeed * Time.deltaTime);
     }
 }
