f9c2e3d234e487c41c15febbb981704afbebd77f
Christoffer Hald Christensen <chrishald@outlook.dk>
2023-10-23 19:09:58 +0200

    Added a SanityClutter script to enable and disable object based on sanity

--- Assets/Scipts/Visual Effects/SanityDecal.cs
+++ Assets/Scipts/Visual Effects/SanityDecal.cs
@@ -17,6 +17,7 @@ public class SanityDecal : MonoBehaviour
     {
     }
 
+    // Start is called before the first frame update
     void Start()
     {
         SanityManager.Instance.OnSanityChange += UpdateVisibility;
